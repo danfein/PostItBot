00515e9938c4abee67963f849b9d3c0b7206bc50
Dan Fein <dpfein@gmail.com>
2014-09-04 23:13:34 -0700

    +16 channel
    added 16 channel in description

--- Prototype2/Prototype2.ino
+++ Prototype2/Prototype2.ino
@@ -2,14 +2,14 @@
   _|_|      _|_|
 -|    _|  _|    _|
 -|    _|  _|    _|
-  _|_|      _|_|           _______
-_|    _|  _|    _|        |  uno  |       
-_|    _|  _|    _|  <==   | +eth  |  <==  WU
-  _|_|      _|_|          |_______|
+  _|_|      _|_|                             _______
+_|    _|  _|    _|                          |  uno  |       
+_|    _|  _|    _|  <==  | 16 chan | <==    | +eth  |  <==  WU
+  _|_|      _|_|                            |_______|
  
 Post-It bot - The Window Weather - Dreamed up by Dan Fein
 This projects connects the Weather Underground API to an Arduino (Ethernet Shield) and displays the day's temperature
-with servo controlled post-it notes in a seven-segment arrangment.
+with servo controlled via an adafruit 16 channel controller with post-it notes attached, in a seven-segment arrangment.
 */
 
 #include <Servo.h>
